0cba8954e9ff357b68ef0ac97375c2e2a2d35214
Dennis319 <dennis.dahl.rasmussen@gmail.com>
2018-05-25 13:34:41 +0200

    Getting SQL code to work... Still in progress

--- AarsprojektSweetHome/AarsprojektSweetHome/Form2.cs
+++ AarsprojektSweetHome/AarsprojektSweetHome/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient; //SQL connection
 
 namespace AarsprojektSweetHome
 {
@@ -14,10 +15,30 @@ namespace AarsprojektSweetHome
     {
         Timer t = new Timer();
 
+        
+
+        // Open a connection to the SQL Server database at a server:
+        SqlConnection conn = new SqlConnection("User id =sweethomedb1; " + // \\ to get \
+                                 "Password=Sn8E~2XHa9_9; " +
+                                 //"Persist Security Info=False;" +
+                                 //"Integrated Security=SSPI;" +
+                                 "Database=sweethomedb1;" +
+                                 "Server=den1.mssql6.gear.host;" + // not localhost
+                                 "Connect Timeout=45");
+
+
+        
+
+
         public Form2()
         {
+
             InitializeComponent();
+            disp_data();
         }
+
+
+
 
         private void Crud_Click(object sender, EventArgs e)
         {
@@ -130,6 +151,91 @@ namespace AarsprojektSweetHome
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }*/
+
+            conn.Open(); //Åbner forbindelse til databasen
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO Boliger2 VALUES('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "','" + textBox6.Text + "','" + textBox7.Text + "','" + textBox8.Text + "','" + textBox9.Text + "','" + textBox10.Text + "','" + textBox11.Text + "','" + textBox12.Text + "','" + textBox13.Text + "','" + textBox14.Text + "','" + textBox15.Text + "','" + textBox16.Text + "')";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            try
+            {
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            disp_data();
+            MessageBox.Show("Oprettelse gennemført");
+        }
+
+        public void disp_data() //Opdatere DataGridView
+        {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * from Boliger2";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            conn.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.Open(); //Åbner forbindelse til databasen
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "DELETE FROM Ordre WHERE OId = '" + textBox1.Text + " ' ";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            disp_data();
+            MessageBox.Show("Sletning gennemført");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            conn.Open(); //Åbner forbindelse til databasen
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE Ordre SET Serienummer = '" + textBox2.Text + "', KId = '" + textBox3.Text + "', AId = '" + textBox4.Text + "', OPris = '" + textBox5.Text + "', Dato = '" + textBox6.Text + "' WHERE OId = '" + textBox1.Text + "'";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            disp_data();
+            MessageBox.Show("Opdatering af gennemført");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            conn.Open(); //Åbner forbindelse til databasen
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM Ordre WHERE Dato BETWEEN '" + textBox1.Text + "' AND '" + textBox2.Text + "'";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
