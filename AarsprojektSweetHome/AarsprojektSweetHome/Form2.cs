1a36052ec2bd69fa365aed469508d5df68b7071b
Dennis319 <dennis.dahl.rasmussen@gmail.com>
2018-05-30 13:07:13 +0200

    "Dennis er den sejeste"

--- AarsprojektSweetHome/AarsprojektSweetHome/Form2.cs
+++ AarsprojektSweetHome/AarsprojektSweetHome/Form2.cs
@@ -312,7 +312,7 @@ namespace AarsprojektSweetHome
         {
             disp_data();
             ClearData();
-            MessageBox.Show("Sync Working 1");
+            MessageBox.Show("Dennis er den sejeste");
         }
 
         private void button7_Click(object sender, EventArgs e)
